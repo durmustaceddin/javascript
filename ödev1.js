e3c6fcba343ad458c986196ec70a488c4f5af5ad
durmustaceddin <109717544+durmustaceddin@users.noreply.github.com>
2022-07-21 09:09:19 +0300

    Add files via upload

---
+++ ödev1.js
@@ -0,0 +1,79 @@
+//  let sonuc = prompt("Bir sayı giriniz", "100");
+// let donusum = parseInt(sonuc);
+
+// let dakika = parseInt ((donusum / 60),10)
+// let saniye = donusum % 60;
+
+
+
+// console.log(`Girdiğiniz ${donusum} değeri: ${dakika} dakika ve ${saniye} saniyedir` );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+Bir değişken oluşturun ve bu değişkende saniye değeri tutun.
+Sonra bu saniye değerinin kaç dakika ve saniyeye denk geldiğini yazdırın
+Örnek 1200 değeri için 20 dakika 0 saniye yazmalıdır */
+
+
+
+// let saniye =     prompt("Bir saniye yaz kanka","61")
+// let dakika= saniye/60
+// // saniye1.toFixed(1)
+
+// let saniye1 = saniye % 60
+
+// // saniye1.toFixed(2)
+// console.log("Girdiğiniz sayı: "+ dakika.toFixed(0)+ " dakika "+saniye1+" saniyedir")
+
+
+// -----------------------------------------------------------------------------------------------------------------------------------------
+
+
+// let girlilen_sayi = prompt("Naber Müdür")
+
+
+
+
+// let dakika = parseInt(girlilen_sayi/60)
+
+// let asd = console.log("Girdiğiniz sayı:"+(dakika)+" dakika "+(girlilen_sayi%60)+" saniyeden oluşmaktadır")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
