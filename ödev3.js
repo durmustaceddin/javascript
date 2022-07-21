7a468da996e8e8fecdc7e612dbe93244e0e828da
durmustaceddin <109717544+durmustaceddin@users.noreply.github.com>
2022-07-21 09:10:12 +0300

    Add files via upload

---
+++ ödev3.js
@@ -0,0 +1,45 @@
+/* Bir yıl değerinin artık yıl olup olmadığını hesaplayın;
+Bir yılın artık olması için ya 400 ile modu 0 olmalı ya da 4 ile modu 0 olmalı e 100 ile
+ modu 0 olmamalı
+ 4100 false değer vermelidir */
+
+//  let girilen_yıl = parseInt(prompt("Bir Yıl Değeri Giriniz", "2022"))
+
+//  let asd = console.log(girilen_yıl % 400 == 0 || girilen_yıl % 4 == 0 && girilen_yıl % 100 != 0 ) 
+
+
+
+
+
+
+/*Bir yıl değerinin artık yıl olup olmadığını hesaplayın;
+Bir yılın artık olması için ya 400 ile modu 0 olmalı ya da 4 ile modu 0 olmalı e 100 ile
+ modu 0 olmamalı
+ 4100 false değer vermelidir */
+
+ 
+
+
+
+
+// let yıl_değeri = prompt("Hangi Yılda Olmak İsterdin","4100")
+
+// let yıl_mı = ((yıl_değeri % 400) == 0)  || ((yıl_değeri %4) == 0 ) && ( (yıl_değeri % 100 )!= 0)
+
+// console.log("Girdiğiniz "+ yıl_değeri+ " Yılı Artık Yıl mı :"+  yıl_mı);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
