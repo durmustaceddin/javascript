f0409ed27bb90a8981054142e61c5db110ef105f
durmustaceddin <109717544+durmustaceddin@users.noreply.github.com>
2022-07-21 09:09:37 +0300

    Add files via upload

---
+++ ödev2.js
@@ -0,0 +1,78 @@
+/*
+Bir değişken oluşturun ve bu değişkende fahrenheit cinsinde sıcaklığı saklayın.
+Sonrasında bu değerin kaç celcius olduğunu hesaplayın.
+100 değeri için 37.777778 gibi bir değer elde etmelisiniz.
+Hesaplama formülü : celcius = 5/9 * (fahrenhit -32) */
+
+
+// let fahrenheit = (prompt("bir_sayı_giriniz","100"))
+// let celcius = (5/9 * (fahrenheit -32))
+// console.log("Girdiğiniz "+ fahrenheit+" fahrenheit sıcaklık "+ celcius.toFixed(2)+" derecedir")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Bir değişken oluşturun ve bu değişkende fahrenheit cinsinde sıcaklığı saklayın.
+Sonrasında bu değerin kaç celcius olduğunu hesaplayın.
+100 değeri için 37.777778 gibi bir değer elde etmelisiniz.
+Hesaplama formülü : celcius = 5/9 * (fahrenhit -32) */
+
+
+
+/* TEKRAR */
+
+
+
+
+
+// let mehmet = alert("Buraya Gir Yazan Yere Bir Sayı Giriniz")
+
+// let ali = prompt("Buraya Gir","Naber Müdür ?")
+// let veli = 5/9 * (ali-32)
+// let tam_sayi = Math.floor(veli)
+
+
+
+// console.log("Girdiğiniz sayı: " + ali +  " fahrenheit sıcaklık "+  tam_sayi + " celcius derecedir" )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
