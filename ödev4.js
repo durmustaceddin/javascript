481bfb855853528003f9ca835cad5d29e0d01b29
durmustaceddin <109717544+durmustaceddin@users.noreply.github.com>
2022-07-21 09:10:28 +0300

    Add files via upload

---
+++ ödev4.js
@@ -0,0 +1,75 @@
+// let rastgele_sayi = (Math.random() * 90) + 10
+
+// rastgele_sayi = Math.floor(rastgele_sayi)
+// console.log("İpucu: " + rastgele_sayi)
+// let sayi_gir = parseInt(prompt("iki basamaklı bir sayı giriniz","10"))
+
+// console.log(`Sistemin ürettiği sayı: ${rastgele_sayi} sizin yazdığınız sayı: ${sayi_gir}`)
+
+// let birler_basamagi = (rastgele_sayi % 10 )
+// let onlar_basamagi = (Math.floor(rastgele_sayi/10))
+
+
+// let user_birler_basamagi = (sayi_gir % 10 )
+// let user_onlar_basamagi = (Math.floor(sayi_gir/10))
+
+
+
+// if(rastgele_sayi===sayi_gir){
+//     console.log("Tahmininiz "+sayi_gir+ " Üretilen Sayı:"+rastgele_sayi)
+//     console.log("Tebrikler 10000 tl kazandınız...")
+// }else if((birler_basamagi === user_onlar_basamagi) && (onlar_basamagi===user_birler_basamagi)){
+//     console.log("Sadece rakamlarını bildiğiniz için 5000tl kazandınız")
+// }else if((birler_basamagi===user_birler_basamagi)||(onlar_basamagi===user_onlar_basamagi)){
+//     console.log("Sadece bir basamağını bildiğiniz için 1000tl kazandınız")
+// }else{
+//     console.log("Malesef bilemediniz")
+// }
+
+
+// for (let i = 0; i < 20; i++){
+//     console.log("Bug")
+
+// }
+
+// for (let i = 0; i < 3; )
+
+
+// for (let i = 0; i < 3; ){
+//     console.log("Döngü çalıştı")
+//     console.log("Nasılsın")
+// }
+
+
+
+for(; ;){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// } else if
+//   let asdad= (birler_basamagi+onlar_basamagi === sayi_gir) 
+//     console.log(`Girdiğiniz sayı: ${sayi_gir} ile rastgele gelen sayı  ${rastgele_sayi} rakamları aynı ama yerleri farklı olduğu için sadece 5000 tl kazandınız`)
+//     console.log(asdad)
